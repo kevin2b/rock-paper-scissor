2285fe5fbce74bf734930dd34a8b86c963dc4871
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-02-26 09:47:16 -0500

    Add function to play five rounds against CPU

--- main.js
+++ main.js
@@ -7,9 +7,52 @@ const map = {
   "scissor": 2
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-console.log(playRound(computerChoice, humanChoice));
+playMatch();
+
+//Play rock, paper, scissor for 5 rounds.
+function playMatch(){
+  let score = 0;
+  for (let i = 0; i < 5; i++){
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice();
+    const result = playRound(computerChoice, humanChoice);
+    score += result[0];
+    console.log(result[1]);
+  }
+  if (score > 0){
+    console.log("You win!")
+  }
+  if(score < 0){
+    console.log("You lose!")
+  }
+  if(score === 0){
+    console.log("Draw!");
+  }
+}
+
+//Choices should be "rock", "paper" or "scissor"
+//Returns an array with two values, first value is a number where -1 is a loss, 0 is a draw and 1 is a win
+//Second value is a game message regarding the result of the round
+function playRound(computerChoice, humanChoice){
+  //Assume row represents computer choice, column human choice.
+  //rock = 0, paper = 1, scissor = 2. -1 means computer win, 0 is draw and 1 means player win
+  const resultMatrix = [[0,1,-1],
+                        [-1,0,1],
+                        [1,-1,0]];
+  
+  const result = resultMatrix[map[computerChoice]][map[humanChoice]];
+
+  computerChoice = capitalizeFirstLetter(computerChoice);
+  humanChoice = capitalizeFirstLetter(humanChoice);
+
+  if (result == -1){
+    return [-1, "You Lose! " + computerChoice + " beats " + humanChoice + "."];
+  }
+  if (result == 1){
+    return [1, "You Win! " + humanChoice + " beats " + computerChoice + "."];
+  }
+  return [0, "Draw! Both players chose " + computerChoice + "."];
+}
 
 function getComputerChoice(){
   //Generate a real number from [0,3)
@@ -29,28 +72,6 @@ function getHumanChoice(){
     answer = answer.toLowerCase();
   } while(map[answer] === undefined);
   return answer;
-}
-
-//Choices should be "rock", "paper" or "scissor"
-function playRound(computerChoice, humanChoice){
-  //Assume row represents computer choice, column human choice.
-  //rock = 0, paper = 1, scissor = 2. -1 means computer win, 0 is draw and 1 means player win
-  const resultMatrix = [[0,1,-1],
-                        [-1,0,1],
-                        [1,-1,0]];
-  
-  const result = resultMatrix[map[computerChoice]][map[humanChoice]];
-
-  computerChoice = capitalizeFirstLetter(computerChoice);
-  humanChoice = capitalizeFirstLetter(humanChoice);
-
-  if (result == -1){
-    return "You Lose! " + computerChoice + " beats " + humanChoice + ".";
-  }
-  if (result == 1){
-    return "You Win! " + humanChoice + " beats " + computerChoice + ".";
-  }
-  return "Draw! Both players chose " + computerChoice + ".";
 }
 
 function capitalizeFirstLetter(word){
