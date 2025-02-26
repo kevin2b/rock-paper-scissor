fcb230f7ff63357d47433708c9dd83554786dca9
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-02-26 08:09:19 -0500

    Add function to randomly generate computer choice

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+
+
+const dictionary = ["rock", "paper", "scissor"];
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+function getComputerChoice(){
+  //Generate a real number from [0,3)
+  let compSelect = Math.random() * 3;
+
+  //Round generated number to 0, 1 or 2 which represents rock, paper and scissor
+  compSelect = Math.floor(compSelect);
+  return dictionary[compSelect];
+}
