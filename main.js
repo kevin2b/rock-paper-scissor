7abf96dfd176d27862f28edc1879c96594f0cc37
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-02-26 08:20:23 -0500

    Add function to prompt for user choice

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 
 const dictionary = ["rock", "paper", "scissor"];
+const map = {
+  "rock": 0,
+  "paper": 1,
+  "scissor": 2
+}
 console.log(getComputerChoice());
-console.log(getComputerChoice());
+console.log(getHumanChoice());
 
 function getComputerChoice(){
   //Generate a real number from [0,3)
@@ -11,4 +16,15 @@ function getComputerChoice(){
   //Round generated number to 0, 1 or 2 which represents rock, paper and scissor
   compSelect = Math.floor(compSelect);
   return dictionary[compSelect];
+}
+
+function getHumanChoice(){
+  let answer;
+
+  //Keep prompting user until they enter rock, paper or scissor case insensitive. Default rock.
+  do{
+    answer = prompt("Enter rock, paper or scissor", "rock");
+    answer = answer.toLowerCase();
+  } while(map[answer] === undefined);
+  return answer;
 }
